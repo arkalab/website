17df1de9279350f9a50abb3ca19790b0ca8ad0e7
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2023-12-07 18:33:22 -0300

    Change description

--- src/app/page.js
+++ src/app/page.js
@@ -14,10 +14,10 @@ export default function Home() {
         <div className="absolute w-3/4 h-fit bottom-48 left-32 font-jost">
           <h1 className="text-8xl font-medium mb-6">ARKA</h1>
           <p className="text-3xl font-light w-3/5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
-            varius libero. Fusce eu suscipit lacus. Aliquam sit amet lorem at
-            est suscipit rutrum. Donec vitae mi et lectus aliquam pulvinar. Cras
-            vestibulum diam id lorem elementum gravida.
+          Somos uma consultoria especializada na gestão de acervos, desenvolvendo soluções de tecnologia 
+          e treinamentos para instituições de memória. Trabalhamos com engenharia de dados, aprendizado de máquina e 
+          aplicações web com foco no acesso e engajamento online.
+
           </p>
         </div>
       </section>
