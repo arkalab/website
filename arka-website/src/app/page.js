f0ee5a5350133d2f94b2550847c75694ff5bf4f8
yuritavares <yuriatp@gmail.com>
2023-12-07 17:28:39 -0300

    Adiciona seção de form com função handler.

--- arka-website/src/app/page.js
+++ arka-website/src/app/page.js
@@ -1,4 +1,13 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setEmail("");
+  };
+
   return (
     <main className="w-screen">
       <section className="w-screen h-screen">
@@ -27,6 +36,33 @@ export default function Home() {
               <li>Martim Passos faz isso e aquilo</li>
               <li>Yuri Tavares faz isso e aquilo</li>
             </ul>
+          </div>
+          <div className="w-1/2 h-fit font-jost mb-20">
+            <h2 className="font-bold text-3xl mb-4">Fique por dentro</h2>
+            <p className="text-2xl">
+              Você quer saber mais, esse texto é tipo texto publicitário
+            </p>
+            <form
+              method="post"
+              onSubmit={handleSubmit}
+              className="flex w-full mt-4"
+            >
+              <input
+                id=""
+                name=""
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Cadastre seu e-mail"
+                className="flex w-full h-12 border-2 border-cyan-500 bg-4 p-2 outline-none"
+              />
+              <button
+                type="submit"
+                className="flex h-12 bg-cyan-500 text-white my-auto place-content-center px-6 items-center"
+              >
+                Enviar
+              </button>
+            </form>
           </div>
         </div>
       </section>
