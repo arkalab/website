fa97222fb56f3b45e8dcf2db7003f53d14bad073
yuritavares <yuriatp@gmail.com>
2023-12-07 17:27:38 -0300

    Adiciona seção de pessoas.

--- arka-website/src/app/page.js
+++ arka-website/src/app/page.js
@@ -20,6 +20,14 @@ export default function Home() {
               <li><a href="https://github.com/martimpassos/tropiiify" target="_blank" className="underline">tropiiify</a> faz isso e aquilo</li>
             </ul>
           </div>
+          <div className="mb-20 font-jost">
+            <h2 className="font-jost font-bold text-3xl mb-4">Pessoas</h2>
+            <ul className="text-2xl space-y-2">
+              <li>Bruno Buccalon faz isso e aquilo</li>
+              <li>Martim Passos faz isso e aquilo</li>
+              <li>Yuri Tavares faz isso e aquilo</li>
+            </ul>
+          </div>
         </div>
       </section>
     </main>
