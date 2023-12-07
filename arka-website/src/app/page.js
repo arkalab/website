5d32040bde669ff5bb80e60492263d88ca5fae04
yuritavares <yuriatp@gmail.com>
2023-12-07 17:25:43 -0300

    Adiciona primeira section, com título e descrição.

--- arka-website/src/app/page.js
+++ arka-website/src/app/page.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <main className="w-screen"></main>;
+  return (
+    <main className="w-screen">
+      <section className="w-screen h-screen">
+        <div className="absolute w-3/4 h-fit bottom-48 left-32 font-jost">
+          <h1 className="text-8xl font-medium mb-6">ARKA</h1>
+          <p className="text-3xl font-light w-3/5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
+            varius libero. Fusce eu suscipit lacus. Aliquam sit amet lorem at
+            est suscipit rutrum. Donec vitae mi et lectus aliquam pulvinar. Cras
+            vestibulum diam id lorem elementum gravida.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
