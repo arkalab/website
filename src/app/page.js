144e74eb900c8f3d84a53e106717579aff42f058
bruno <buccalon@users.noreply.github.com>
2023-12-09 07:27:56 -0600

    Update content and minor layout adjustments

--- src/app/page.js
+++ src/app/page.js
@@ -13,10 +13,10 @@ export default function Home() {
       <section className="w-screen h-screen">
         <div className="absolute w-3/4 h-fit bottom-48 left-32 font-jost">
           <h1 className="text-8xl font-medium mb-6">ARKA</h1>
-          <p className="text-3xl font-light w-3/5">
+          <p className="text-2xl font-light w-3/5">
           Somos uma consultoria especializada na gestão de acervos, desenvolvendo soluções de tecnologia 
           e treinamentos para instituições de memória. Trabalhamos com engenharia de dados, aprendizado de máquina e 
-          aplicações web com foco no acesso e engajamento online.
+          aplicações web com foco em acesso aberto e engajamento online.
 
           </p>
         </div>
@@ -26,21 +26,21 @@ export default function Home() {
           <div className="mb-20 font-jost">
             <h2 className="font-jost font-bold text-3xl mb-4">Projetos</h2>
             <ul className="text-2xl space-y-2">
-              <li><a href="https://github.com/martimpassos/tropiiify" target="_blank" className="underline">tropiiify</a> faz isso e aquilo</li>
+              <li><a href="https://github.com/martimpassos/tropiiify" target="_blank" className="underline">tropiiify</a> exporta projetos Tropy como coleções IIIF</li>
             </ul>
           </div>
           <div className="mb-20 font-jost">
             <h2 className="font-jost font-bold text-3xl mb-4">Pessoas</h2>
             <ul className="text-2xl space-y-2">
-              <li>Bruno Buccalon faz isso e aquilo</li>
-              <li>Martim Passos faz isso e aquilo</li>
-              <li>Yuri Tavares faz isso e aquilo</li>
+              <li><a href="https://bruno.land/" target="_blank" className="underline">Bruno Buccalon</a> é pesquisador e designer</li>
+              <li>Martim Passos é arquiteto e programador</li>
+              <li>Yuri Tavares é jornalista e programador</li>
             </ul>
           </div>
           <div className="w-1/2 h-fit font-jost mb-20">
-            <h2 className="font-bold text-3xl mb-4">Fique por dentro</h2>
+            <h2 className="font-bold text-3xl mb-4">Saiba mais</h2>
             <p className="text-2xl">
-              Você quer saber mais, esse texto é tipo texto publicitário
+              Cadastre seu e-mail para receber novidades
             </p>
             <form
               method="post"
@@ -53,7 +53,7 @@ export default function Home() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 type="email"
-                placeholder="Cadastre seu e-mail"
+                placeholder="E-mail"
                 className="flex w-full h-12 border-2 border-cyan-500 bg-4 p-2 outline-none"
               />
               <button
