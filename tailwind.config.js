a273a8cc0726842be0e5b2e79ac009d7785077b9
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-01-09 14:07:42 -0300

    Enable Tailwind Jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
