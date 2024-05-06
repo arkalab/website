b89d28ed5ab181cad4886105bd005dae5316f212
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-05-06 16:22:58 -0300

    Center text

--- components/header.jsx
+++ components/header.jsx
@@ -2,11 +2,10 @@ export default function Header({ children }) {
     return <>
         <section id="home" className="flex flex-col relative">
             <img src="/image.png" alt="Description of image" className="w-screen h-screen object-cover" />
-            <div className="flex items-start left-[20vw] right-[20vw] bottom-[25vh] absolute w-[80vw]">
-                <img src="/logo.svg" alt="Arka logo" className="relative w-60 max-w-[40vw] left-0 top-0"/>
+            <div className="flex items-start mx-auto bottom-[25vh] justify-center absolute left-0 right-0">
+                <img src="/logo.svg" alt="Arka logo" className="relative w-60 max-w-[40vw]"/>
                 <div className="relative max-w-[39vw] top-14 ml-6 text-white text-xl md:text-2xl font-jost">{children}</div>
             </div>
         </section>
     </>
 }
-
