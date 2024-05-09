73c6e3d4b3b2bf931fc1ab478a88e41546852c15
yuritavares <yuriatp@gmail.com>
2024-05-09 18:52:51 -0300

    ContentBoxTitle: Creating new component.

---
+++ components/ContentBoxTitle.jsx
@@ -0,0 +1,5 @@
+export default function ContentBoxTitle({ children }) {
+  return (
+    <div className="place-content-center w-full h-14 bg-[#06191f] rounded-t-lg text-2xl font-bold px-8 mb-1">{children}</div>
+  )
+}
