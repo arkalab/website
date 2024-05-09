13d28daddb2e66932caf69425f0006e7bbc2ed09
yuritavares <yuriatp@gmail.com>
2024-05-09 19:02:13 -0300

    ContentBoxTeamMember: Adding team member name and job title.

--- components/ContentBoxTeamMember.jsx
+++ components/ContentBoxTeamMember.jsx
@@ -6,6 +6,10 @@ export default function ContentBoxPhoto({ img, name, jobTitle }) {
       <div className="mb-4">
         <img className="rounded-lg grayscale" src={img} />
       </div>
+      <div className="text-center">
+        <p className="font-extrabold">{name}</p>
+        <p className="italic">{jobTitle}</p>
+      </div>
     </div>
   )
 }
