68936ffb77b0ce142852f4c65aee447687d335d8
yuritavares <yuriatp@gmail.com>
2024-05-09 17:41:33 -0300

    Adding Jost font to tailwind config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
     './components/**/*.{js,jsx,ts,tsx,md,mdx}',
- 
+
     // Or if using `src` directory:
     './src/**/*.{js,jsx,ts,tsx,md,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      // Adding custom fonts
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+    }
   },
   plugins: []
 }
