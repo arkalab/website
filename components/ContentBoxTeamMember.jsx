5d228c1465f7d9b5789e45ff853d075eb37ae4f5
yuritavares <yuriatp@gmail.com>
2024-05-09 19:01:55 -0300

    ContentBoxTeamMember: Adding team member photo.

--- components/ContentBoxTeamMember.jsx
+++ components/ContentBoxTeamMember.jsx
@@ -2,6 +2,10 @@ import Image from "next/image"
 
 export default function ContentBoxPhoto({ img, name, jobTitle }) {
   return (
-    <div></div>
+    <div>
+      <div className="mb-4">
+        <img className="rounded-lg grayscale" src={img} />
+      </div>
+    </div>
   )
 }
