b9542a13506b95678458d369d80c382c4a729398
yuritavares <yuriatp@gmail.com>
2024-05-27 20:16:17 -0300

    ContentBoxProject: Adding project logo image.

--- components/ContentBoxProject.jsx
+++ components/ContentBoxProject.jsx
@@ -2,6 +2,7 @@ export default function ContentBoxProject({ logo, img, name, url, description })
   return (
     <div className="w-full">
       <div className="relative w-full mb-4">
+        <img src={logo} className="absolute left-5 bottom-5 z-10"/>
         <img src={img} className="w-full rounded-lg" />
       </div>
     </div>
