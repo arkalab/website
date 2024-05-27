de346ec7ae3cb0db7a9fcaac874c07ee3d553b9d
yuritavares <yuriatp@gmail.com>
2024-05-27 20:34:55 -0300

    ContentBoxPartner: Creating new component.

--- components/ContentBoxTeamMember.jsx
+++ components/ContentBoxTeamMember.jsx
@@ -2,7 +2,7 @@ export default function ContentBoxTeamMember({ img, name, jobTitle }) {
   return (
     <div>
       <div className="mb-4">
-        <img className="w-full rounded-lg" src={img} />
+        <img src={img} className="w-full rounded-lg" />
       </div>
       <div className="text-center">
         <p className="font-extrabold">{name}</p>
