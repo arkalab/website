a78f9f5f0459bf718bced3eb0e4594db13ad1cc1
yuritavares <yuriatp@gmail.com>
2024-05-27 20:13:56 -0300

    ContentBoxTeamMember: Small adjustments.

--- components/ContentBoxTeamMember.jsx
+++ components/ContentBoxTeamMember.jsx
@@ -1,10 +1,8 @@
-import Image from "next/image"
-
-export default function ContentBoxPhoto({ img, name, jobTitle }) {
+export default function ContentBoxTeamMember({ img, name, jobTitle }) {
   return (
     <div>
       <div className="mb-4">
-        <img className="rounded-lg grayscale" src={img} />
+        <img className="w-full rounded-lg" src={img} />
       </div>
       <div className="text-center">
         <p className="font-extrabold">{name}</p>
