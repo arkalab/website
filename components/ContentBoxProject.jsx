d3dc0107b34b02cd709214d3dde9b9fc01f133c1
yuritavares <yuriatp@gmail.com>
2024-05-27 20:16:02 -0300

    ContentBoxProject: Adding main image.

--- components/ContentBoxProject.jsx
+++ components/ContentBoxProject.jsx
@@ -1,5 +1,9 @@
 export default function ContentBoxProject({ logo, img, name, url, description }) {
   return (
-    <div className="w-full"></div>
+    <div className="w-full">
+      <div className="relative w-full mb-4">
+        <img src={img} className="w-full rounded-lg" />
+      </div>
+    </div>
   )
 }
