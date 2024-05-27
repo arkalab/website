0e6be80d4c2bfeeb5960727c248906f7096c3efe
yuritavares <yuriatp@gmail.com>
2024-05-27 20:42:29 -0300

    ContentBoxTeamMember: Adding hyperlink to team member name if url prop is passed.

--- components/ContentBoxTeamMember.jsx
+++ components/ContentBoxTeamMember.jsx
@@ -1,13 +1,19 @@
-export default function ContentBoxTeamMember({ img, name, jobTitle }) {
+export default function ContentBoxTeamMember({ img, name, jobTitle, url }) {
   return (
     <div>
       <div className="mb-4">
         <img src={img} className="w-full rounded-lg" />
       </div>
       <div className="text-center">
-        <p className="font-extrabold">{name}</p>
-        <p className="italic">{jobTitle}</p>
+        {url === "" ? (
+          <p className="font-extrabold">{name}</p>
+        ) : (
+          <a href={url} target="_blank">
+            <p className="font-extrabold">{name}</p>
+          </a>
+        )}
+        <p>{jobTitle}</p>
       </div>
     </div>
-  )
+  );
 }
