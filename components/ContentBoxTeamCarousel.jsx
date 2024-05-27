093f2e30dcb2c0faac0978fd4576486ce99f3ddd
yuritavares <yuriatp@gmail.com>
2024-05-27 20:12:20 -0300

    ContentBoxTeamCarousel: Deleting component.

--- components/ContentBoxTeamCarousel.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContentBoxPhotoCarousel({ children }) {
-  return (
-    <div className="grid grid-cols-4 gap-10 w-full h-fit bg-[#06191f] rounded-b-lg font-light px-12 py-8">
-      {children}
-    </div>
-  )
-}
