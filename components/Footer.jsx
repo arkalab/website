ecbb1b3dd40ec460faec31b3053146589af08576
yuritavares <yuriatp@gmail.com>
2024-05-27 21:05:56 -0300

    Footer: Styling component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer({ children }) {
   return (
-    <div>
+    <div className="grid w-full h-96 bg-[#06191f] font-jost text-white text-xl text-center place-content-center space-y-2">
       {children}
     </div>
   )
