c653621c397a46a4a29b011ac16adcdb440a1b19
yuritavares <yuriatp@gmail.com>
2024-05-27 22:19:33 -0300

    ContentBoxCarousel: Splitting component into two.

--- components/ContentBoxCarousel.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContentBoxCarousel({ children, columns }) {
-  return (
-    <div className={"grid grid-cols-" + columns.toString() + " gap-10 w-full h-fit bg-[#06191f] rounded-b-lg font-light px-12 py-8"}>
-      {children}
-    </div>
-  )
-}
---
+++ components/ContentBoxCarouselFourColumns.jsx
@@ -0,0 +1,7 @@
+export default function ContentBoxCarouselFourColumns({ children }) {
+  return (
+    <div className={"grid grid-cols-4 gap-10 w-full h-fit bg-[#06191f] rounded-b-lg font-light px-12 py-8"}>
+      {children}
+    </div>
+  )
+}
