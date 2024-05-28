aea9aee81dda1b819471109dd384b0391ff1d6ec
yuritavares <yuriatp@gmail.com>
2024-05-27 21:06:49 -0300

    MainContent: Small fix.

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -1,6 +1,6 @@
 export default function MainContent({ children }) {
   return (
-    <div className="w-screen h-fit bg-[#01242c] font-jost text-white py-20">
+    <div className="w-full h-fit bg-[#01242c] font-jost text-white py-20">
       {children}
     </div>
   )
