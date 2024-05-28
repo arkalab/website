f139b14af6f498c855e6efbc8ecd653f20a3498f
yuritavares <yuriatp@gmail.com>
2024-05-27 22:56:58 -0300

    ContentBoxText: Changing the way to define font size.

--- components/ContentBoxText.jsx
+++ components/ContentBoxText.jsx
@@ -1,3 +1,8 @@
-export default function ContentBoxText({ children, fontSize }) {
+import React from 'react';
+
+export default function ContentBoxText({ children }) {
+  const countChildren = React.Children.count(children);
+  const fontSize = countChildren > 1 ? "xl" : "2xl"; 
+
   return <div className={"text-" + fontSize + " space-y-6"}>{children}</div>;
 }
