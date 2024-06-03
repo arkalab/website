8dd5ecca8ab3835a497730b939793656f9b09bf0
yuritavares <yuriatp@gmail.com>
2024-06-03 18:26:23 -0300

    ContentBox: Adding style for mobile version.

--- components/ContentBox.jsx
+++ components/ContentBox.jsx
@@ -1,6 +1,6 @@
 export default function ContentBox({ children }) {
   return (
-    <div className="w-8/12 mx-auto mb-16">
+    <div className="w-10/12 sm:w-8/12 mx-auto mb-20 sm:mb-16">
       {children}
     </div>
   )
