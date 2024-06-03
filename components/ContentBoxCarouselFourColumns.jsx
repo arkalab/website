c2821e1392fee0f43c7ff1d73ebe294ef49e159b
yuritavares <yuriatp@gmail.com>
2024-06-03 18:27:58 -0300

    ContentBoxCarouselFourColumns: Adding style for mobile version.

--- components/ContentBoxCarouselFourColumns.jsx
+++ components/ContentBoxCarouselFourColumns.jsx
@@ -1,6 +1,6 @@
 export default function ContentBoxCarouselFourColumns({ children }) {
   return (
-    <div className="grid grid-cols-4 gap-10 w-full h-fit bg-[#06191f] rounded-b-lg font-light px-12 py-8">
+    <div className="grid grid-cols-2 sm:grid-cols-4 gap-10 w-full h-fit bg-[#06191f] rounded-b-lg font-light px-12 py-8">
       {children}
     </div>
   )
