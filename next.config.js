2e0f085bb1bae2172aaa62b9282953e11a31a10f
yuritavares <yuriatp@gmail.com>
2024-06-04 14:05:45 -0300

    Changing configfiles to deploy to Github Pages (#01).

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@ const withNextra = require('nextra')({
   theme: './theme.tsx',
   themeConfig: './theme.config.jsx'
 })
- 
+
 module.exports = withNextra({
-    i18n: {
-      locales: ['en', 'pt'],
-      defaultLocale: 'en'
-    }
-  })
- 
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en'
+  },
+  images: {
+    unoptimized: true,
+  },
+})
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
