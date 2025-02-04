9e1075fe1cba821cd1a2acadf76ee936208a88f4
Yuri Tavares <yuriatp@gmail.com>
2025-02-04 18:20:56 -0300

    Removing unused files from nextra.

--- theme.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextraThemeLayoutProps } from "nextra";
-
-export default function Layout({ children }: NextraThemeLayoutProps) {
-  return (
-    <div>
-      <div>{children}</div>
-    </div>
-  );
-}
